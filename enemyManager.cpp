9b7d457559a842877297d1aa820c02469bf7b3cc
pyonnu <55772465+pyonnu@users.noreply.github.com>
2019-12-30 15:30:18 +0900

    적 위치 잡아놓

--- enemyManager.cpp
+++ enemyManager.cpp
@@ -17,20 +17,8 @@ HRESULT EnemyManager::init()
 		(*_viEnemy)->init();
 		
 	}*/
-	/*Enemy* Mummy;
-	Mummy = new Enemy_Mummy;
-	Mummy->init(EnemyType::MUMMY,83, 750);
-	_vEnemy.push_back(Mummy);*/
-
-	/*Enemy* Scorpion;
-	Scorpion = new Enemy_Scorpion;
-	Scorpion->init(EnemyType::SCORPION, 83, 750);
-	_vEnemy.push_back(Scorpion);*/
-
-	Enemy* Snake;
-	Snake = new Enemy_Snake;
-	Snake->init(EnemyType::SNAKE, 83, 750);
-	_vEnemy.push_back(Snake);
+	//적을 생성하는 함수
+	enemyCreate();
 
 	return S_OK;
 }
@@ -58,6 +46,66 @@ void EnemyManager::render()
 
 void EnemyManager::enemyCreate()
 {
+	Enemy* Mummy_1;
+	Mummy_1 = new Enemy_Mummy;
+	Mummy_1->init(EnemyType::MUMMY, 83, 750);
+	_vEnemy.push_back(Mummy_1);
+
+	Enemy* Mummy_2;
+	Mummy_2 = new Enemy_Mummy;
+	Mummy_2->init(EnemyType::MUMMY, 2380, 270);
+	_vEnemy.push_back(Mummy_2);
+
+	Enemy* Mummy_3;
+	Mummy_3 = new Enemy_Mummy;
+	Mummy_3->init(EnemyType::MUMMY, 3179, 185);
+	_vEnemy.push_back(Mummy_3);
+
+	Enemy* Mummy_4;
+	Mummy_4 = new Enemy_Mummy;
+	Mummy_4->init(EnemyType::MUMMY, 3697, 955);
+	_vEnemy.push_back(Mummy_4);
+
+	Enemy* Mummy_5;
+	Mummy_5 = new Enemy_Mummy;
+	Mummy_5->init(EnemyType::MUMMY, 3340, 1240);
+	_vEnemy.push_back(Mummy_5);
+
+	Enemy* Scorpion_1;
+	Scorpion_1 = new Enemy_Scorpion;
+	Scorpion_1->init(EnemyType::SCORPION, 1440, 375);
+	_vEnemy.push_back(Scorpion_1);
+
+	Enemy* Scorpion_2;
+	Scorpion_2 = new Enemy_Scorpion;
+	Scorpion_2->init(EnemyType::SCORPION, 2063, 950);
+	_vEnemy.push_back(Scorpion_2);
+
+	Enemy* Scorpion_3;
+	Scorpion_3 = new Enemy_Scorpion;
+	Scorpion_3->init(EnemyType::SCORPION, 3246, 1245);
+	_vEnemy.push_back(Scorpion_3);
+	
+	Enemy* Snake_1;
+	Snake_1 = new Enemy_Snake;
+	Snake_1->init(EnemyType::SNAKE, 1059, 380);
+	_vEnemy.push_back(Snake_1);
+
+	Enemy* Snake_2;
+	Snake_2 = new Enemy_Snake;
+	Snake_2->init(EnemyType::SNAKE, 2515, 615);
+	_vEnemy.push_back(Snake_2);
+
+	Enemy* Snake_3;
+	Snake_3 = new Enemy_Snake;
+	Snake_3->init(EnemyType::SNAKE, 2751, 615);
+	_vEnemy.push_back(Snake_3);
+	
+
+	/*Enemy* Snake;
+	Snake = new Enemy_Snake;
+	Snake->init(EnemyType::SNAKE, 83, 750);
+	_vEnemy.push_back(Snake);*/
 }
 
 
